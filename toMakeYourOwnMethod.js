323742a293f42833d7c2b09aa8cfea0740ac5524
naman <namanagg.2010@gmail.com>
2021-05-26 20:50:05 +0530

    add camera app

--- toMakeYourOwnMethod.js
+++ toMakeYourOwnMethod.js
@@ -1,3 +1,8 @@
+//every object constructor in js have a prototype and prototype is nothing but an object and now I can implement 
+//any property or method on this prototype
+
+//very imp concept
+
 //thats how we have to write if we have to create our own method or infact override an existing method
 //the use of prototype word is very important
 
