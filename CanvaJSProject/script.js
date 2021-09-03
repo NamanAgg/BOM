874554f42ad6f1778388c858c91414fa411dc350
nmn <namanagg.2010@gmail.com>
2021-09-04 03:56:10 +0530

    commit

--- CanvaJSProject/script.js
+++ CanvaJSProject/script.js
@@ -1,102 +1,155 @@
 let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-let ctx = canvas.getContext("2d"); //this is to createpen or stylus for canvas
-let triangles=[];
-let colors=["red","green","blue","orange","yellow","purple","pink"];
-
-
+let clearBtn = document.querySelector("button");
+let ctx = canvas.getContext("2d");
+let triangles = [];
+let colors = ["red", "green", "blue", "orange", "yellow", "purple", "pink"];
 let startPoint;
 let endPoint;
 let idxOfColor;
-// let width;
-// let height;
+let idx;
+let dragStartPoint;
+
+
+clearBtn.addEventListener("click", function (e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    triangles = [];
+})
 canvas.addEventListener("mousemove", function (e) {
     if (e.buttons == 1) {
-        
-        if (startPoint) {
-            // ctx.beginPath();
-            // ctx.strokeStyle = "blue";
-            // ctx.lineWidth = 5;
-            //  width = e.pageX - startPoint[0];
-            // ctx.fillStyle = "red";
-            // ctx.fillRect(startPoint[0], startPoint[1], width, height);
-            // console.log(startPoint[0], startPoint[1], width, height," these are the values when formed");
-            // console.log(rects);
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            for(let tri of triangles)
-                draw([tri[0],tri[1]],[tri[2],tri[3]],tri[4]);
-            
-            endPoint=[e.pageX,e.pageY];
-            draw(startPoint,endPoint,idxOfColor);
-            
-        } else {
-            startPoint = [e.pageX, e.pageY];
-            idxOfColor = Math.floor(Math.random() * 7);
-            // for(let tri of triangles)
-            //     draw([tri[0],tri[1]],[tri[2],tri[3]]);
-            
+        let pos = getMousePos(canvas, e);
+        if (idx == undefined) getPosForDrag(pos.pageX, pos.pageY);
+        if (idx != undefined) {
+            if (dragStartPoint) {
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                let diff = getDiff(dragStartPoint, [pos.pageX, pos.pageY]);
+                dragStartPoint[0] = pos.pageX;
+                dragStartPoint[1] = pos.pageY;
+                triangles[idx][0] += diff[0];
+                triangles[idx][1] += diff[1];
+                triangles[idx][2] += diff[0];
+                triangles[idx][3] += diff[1];
+                for (let tri of triangles)
+                    draw([tri[0], tri[1]], [tri[2], tri[3]], tri[4]);
+
+
+            }
+            else {
+                let pos = getMousePos(canvas, e);
+                if (dragStartPoint == undefined)
+                    dragStartPoint = [pos.pageX, pos.pageY];
+            }
         }
-    } 
+        else {
+            if (startPoint) {
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                for (let tri of triangles)
+                    draw([tri[0], tri[1]], [tri[2], tri[3]], tri[4]);
+
+                let pos = getMousePos(canvas, e);
+                endPoint = [pos.pageX, pos.pageY];
+                draw(startPoint, endPoint, idxOfColor);
+
+            }
+            else {
+                let pos = getMousePos(canvas, e);
+                startPoint = [pos.pageX, pos.pageY];
+                idxOfColor = Math.floor(Math.random() * 7);
+                for (let tri of triangles)
+                    draw([tri[0], tri[1]], [tri[2], tri[3]], tri[4]);
+
+            }
+        }
+    }
     else {
-         if(startPoint!=undefined && endPoint!=undefined){
-            triangles.push([...startPoint,...endPoint,idxOfColor]);
-         console.log(triangles);
-        startPoint = undefined;
-        endPoint=undefined;
-         }
+        if (startPoint != undefined && endPoint != undefined) {
+            triangles.push([...startPoint, ...endPoint, idxOfColor]);
+            console.log(triangles);
+            startPoint = undefined;
+            endPoint = undefined;
+            for (let tri of triangles)
+                draw([tri[0], tri[1]], [tri[2], tri[3]], tri[4]);
+        }
+        dragStartPoint = undefined;
+        idx = undefined;
     }
 });
 
-canvas.addEventListener("dblclick",function(e){
-    let idx = getPos(e.pageX,e.pageY);
-    if(idx!=undefined){
-        triangles.splice(idx,1);
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        for(let tri of triangles)
-            draw([tri[0],tri[1]],[tri[2],tri[3]],tri[4]);
+
+canvas.addEventListener("dblclick", function (e) {
+    let pos = getMousePos(canvas, e);
+    let idx = getPos(pos.pageX, pos.pageY);
+    if (idx != undefined) {
+        triangles.splice(idx, 1);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let tri of triangles)
+            draw([tri[0], tri[1]], [tri[2], tri[3]], tri[4]);
 
     }
 });
 
-function getPos(clickedPointX,clickedPointY){
-    for(let i=0;i<triangles.length;i++){
-        let start=[triangles[i][0],triangles[i][1]];
-        let end=[triangles[i][2],triangles[i][3]];
-        tempTriangle(start,end);
-        if(ctx.isPointInPath(clickedPointX,clickedPointY)){
+
+function getPos(clickedPointX, clickedPointY) {
+    for (let i = 0; i < triangles.length; i++) {
+        let start = [triangles[i][0], triangles[i][1]];
+        let end = [triangles[i][2], triangles[i][3]];
+        tempTriangle(start, end);
+        if (ctx.isPointInPath(clickedPointX, clickedPointY)) {
             return i;
         }
     }
-    
-    // for(let i=0;i<rects.length;i++){
-    //     let arr = rects[i];
-    //     if(arr[0]<=clickedPointX && arr[1]<=clickedPointY && arr[0]+arr[2]>=clickedPointX && arr[1]+arr[3]>=clickedPointY){
-    //         console.log(arr, "in this triangle db1 will occur");
-    //          rects.splice(i,1);
-    //          return arr;
-    //         console.log(rects);
-    //     }
-    // }
 }
 
 
-function draw(start,end,idxOfColor){
+function draw(start, end, idxOfColor) {
     ctx.beginPath();
-    ctx.moveTo(start[0],start[1]);
-    ctx.lineTo(end[0],end[1]);
-    ctx.lineTo(start[0]+start[0]-end[0],end[1]);
+    ctx.moveTo(start[0], start[1]);
+    ctx.lineTo(end[0], end[1]);
+    ctx.lineTo(start[0] + start[0] - end[0], end[1]);
     ctx.closePath();
     ctx.stroke();
     ctx.fillStyle = colors[idxOfColor];
     ctx.fill();
 }
 
-function tempTriangle(start,end){
+
+function tempTriangle(start, end) {
     ctx.beginPath();
-    ctx.moveTo(start[0],start[1]);
-    ctx.lineTo(end[0],end[1]);
-    ctx.lineTo(start[0]+start[0]-end[0],end[1]);
+    ctx.moveTo(start[0], start[1]);
+    ctx.lineTo(end[0], end[1]);
+    ctx.lineTo(start[0] + start[0] - end[0], end[1]);
     ctx.closePath()
+}
+
+
+function getDiff(point, origin) {
+    return [origin[0] - point[0], origin[1] - point[1]];
+}
+
+
+function getPosForDrag(clickedPointX, clickedPointY) {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (!startPoint)
+        for (let i = 0; i < triangles.length; i++) {
+            let start = [triangles[i][0], triangles[i][1]];
+            let end = [triangles[i][2], triangles[i][3]];
+            tempTriangle(start, end);
+            if (ctx.isPointInPath(clickedPointX, clickedPointY)) {
+                idx = i;
+            }
+        }
+}
+
+function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect(),
+        scaleX = canvas.width / rect.width,
+        scaleY = canvas.height / rect.height;
+
+    return {
+        pageX: (e.clientX - rect.left) * scaleX,
+        pageY: (e.clientY - rect.top) * scaleY
+    }
 }
